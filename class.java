95d1eda879d16d7f0ab65992c363a2f5ad7bc53d
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 17:41:21 +0530

    Create class.java

---
+++ class.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class add {
+
+public static void main(String r[])
+
+{
+
+	int a,b,c;
+
+	Scanner s=new Scanner(System.in);
+
+	System.out.println("Enter the A value=");
+
+	a=s.nextInt();
+
+	System.out.println("Enter the B value=");
+
+	b=s.nextInt();
+
+	c=a+b;
+
+	System.out.println("The Sum of "+a+"+"+b+"="+c);
+
+	
+
+		
+
+}
+
+}
