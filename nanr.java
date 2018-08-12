e39cadba58455741437f81fd6d458d0eb5d23044
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 17:34:16 +0530

    Create nanr.java

---
+++ nanr.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Nanr {
+
+int a,b,c;
+
+Scanner s=new Scanner(System.in);
+
+public void add()
+
+{
+
+	System.out.println("Enter the A value=");
+
+	a=s.nextInt();
+
+	System.out.println("Enter the B value=");
+
+	b=s.nextInt();
+
+	c=a+b;
+
+}
+
+public void sum()
+
+{
+
+	System.out.println("The Sum of "+a+"+"+b+"="+c);
+
+}
+
+public static void main(String ar[])
+
+{
+
+	Nanr d=new Nanr();
+
+	d.add();
+
+	d.sum();
+
+}
+
+}
+
