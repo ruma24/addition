031e46dbdbe781876e8f9348962f613255587f86
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 17:38:05 +0530

    Create wanr.java

---
+++ wanr.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class wanr {
+
+	int a,b,c;
+
+	Scanner s=new Scanner(System.in);
+
+	public void add(int a,int b)
+
+	{
+
+		this.a=a;
+
+		this.b=b;
+
+		c=a+b;
+
+		System.out.println("The Sum of "+a+"+"+b+"="+c);
+
+	}
+
+	public static void main(String ar[])
+
+	{
+
+		wanr b=new wanr();
+
+		b.add(5,6);
+
+	}
+
+}
