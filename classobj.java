a59135cc53784e0dffc64b873e55b8ca4d3b733a
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 17:43:32 +0530

    Create classobj.java

---
+++ classobj.java
@@ -0,0 +1,23 @@
+public class class_obj {
+
+int a,b;
+
+public static void main(String ar[])
+
+{
+
+	class_obj ob =new class_obj();
+
+	ob.a=10;
+
+	ob.b=20;
+
+	int c=ob.a+ob.b;
+
+	System.out.println("The Sum of "+ob.a+"+"+ob.b+"="+c);
+
+	
+
+}
+
+}
